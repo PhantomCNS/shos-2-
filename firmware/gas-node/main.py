29e16c6384f40d59af6046ba2b35a2b86cc37aa3
PhantomCNS2 <dangerman01013@gmail.com>
2026-07-28 04:02:50 +0300

    Enhance gas leak detection: log event to Blynk and manage alarm state

--- firmware/gas-node/main.py
+++ firmware/gas-node/main.py
@@ -25,43 +25,57 @@ blynk = BlynkLib.Blynk(blynk_auth_token,
     insecure = True
     )
 
+alarm_sent = False
+
 
 # Main loop
 while True:
+
     if not wlan.isconnected():
         connect_to_wifi(WiFi_SSID, WiFi_PASSWORD)
+        blynk = BlynkLib.Blynk(blynk_auth_token,
+            tmpl_id = BLYNK_TEMPLATE_ID,
+            insecure = True
+            )
+
     # Read gas sensor value
     gas_value = GAS_SENSOR.value()
-    
     # Read DHT sensor value 
     dht_sensor.measure()
     dht_temp = dht_sensor.temperature()  
     dht_humidity = dht_sensor.humidity() 
 
+    blynk.run()  # Process Blynk events
+    
     # Control relay and alert pattern based on gas sensor value
-    if gas_value == 0: 
+    if gas_value == 0:
         LED.on()
         play_gas_alert(cycles=2)
         stop_buzzer()
         RELAY.on()
         gas_state = "!!GAS LEAK DETECTED!!"
 
+        if not alarm_sent:
+            blynk.log_event("gas_leak", "Gas leak detected in kitchen")
+            alarm_sent = True
+
     else:
         LED.off()
         stop_buzzer()
         RELAY.off()
         gas_state = "Gas levels normal"
-    
+
+        alarm_sent = False            
+
     # Print sensor values for debugging
     print("Gas Sensor Value:", gas_value)
     print("DHT Humidity Value:", dht_humidity)
     print("DHT Temperature Value:", dht_temp)
 
     # Send sensor values to Blynk
-    blynk.run()  # Process Blynk events
     blynk.virtual_write(0, gas_state)  # Send gas sensor value to virtual pin V1
-    blynk.virtual_write(1, dht_humidity)  # Send humidity value to virtual pin V2
-    blynk.virtual_write(2, dht_temp)  # Send temperature value to virtual pin V3
+    blynk.virtual_write(2, dht_humidity)  # Send humidity value to virtual pin V2
+    blynk.virtual_write(1, dht_temp)  # Send temperature value to virtual pin V3
 
     # Wait for a second before the next reading
     time.sleep(1)
