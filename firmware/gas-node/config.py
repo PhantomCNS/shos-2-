9bd2916eac491d49b82158121c7f15ee4dda5b74
PhantomCNS2 <dangerman01013@gmail.com>
2026-07-28 03:02:34 +0300

    Add Blynk integration and enhance main loop for sensor readings

--- firmware/gas-node/config.py
+++ firmware/gas-node/config.py
@@ -2,15 +2,18 @@ from machine import Pin
 import time
 import dht
 import network
-
-# Pin Configurations
+#define BLYNK_TEMPLATE_ID "TMPL2663gkE68"
+#define BLYNK_TEMPLATE_NAME "SHOS 1"
+#define BLYNK_AUTH_TOKEN "PtZuWfyPnv1tC9UJH8jtw85UmL-z9QTG"# Pin Configurations
 gas_sensor_pin = 21
 dht_sensor_pin = 4
 relay_pin = 18
 Buzzer_pin = 19
 LED_pin = 2
-WiFi_SSID = "Your_WiFi_SSID"
-WiFi_PASSWORD = "Your_WiFi_Password"
+BLYNK_TEMPLATE_ID = "TMPL2663gkE68"
+blynk_auth_token = "PtZuWfyPnv1tC9UJH8jtw85UmL-z9QTG"
+WiFi_SSID = "Wokwi-GUEST"
+WiFi_PASSWORD = ""
 
 # Pin Objects
 GAS_SENSOR = Pin(gas_sensor_pin, Pin.IN)
--- firmware/gas-node/main.py
+++ firmware/gas-node/main.py
@@ -1,22 +1,41 @@
 from machine import Pin
 import time
-from config import GAS_SENSOR, DHT_SENSOR, RELAY, LED
+from config import GAS_SENSOR, DHT_SENSOR, RELAY, LED, BLYNK_TEMPLATE_ID, blynk_auth_token, WiFi_SSID, WiFi_PASSWORD
 from buzzer import play_gas_alert, stop_buzzer
 import network
 import dht 
+import BlynkLib
 
 
 dht_sensor = dht.DHT22(DHT_SENSOR)
 
+# Initialize network and connect to WiFi
+wlan = network.WLAN(network.STA_IF)
+def connect_to_wifi(ssid, password):
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while not wlan.isconnected():
+        time.sleep(1)
+    print("Connected to WiFi:", wlan.ifconfig())
 
+connect_to_wifi(WiFi_SSID, WiFi_PASSWORD)
+# Initialize Blynk
+blynk = BlynkLib.Blynk(blynk_auth_token,
+    tmpl_id = BLYNK_TEMPLATE_ID,
+    insecure = True
+    )
+
+
+# Main loop
 while True:
-
+    if not wlan.isconnected():
+        connect_to_wifi(WiFi_SSID, WiFi_PASSWORD)
     # Read gas sensor value
     gas_value = GAS_SENSOR.value()
     
     # Read DHT sensor value 
     dht_sensor.measure()
-    dht_value = dht_sensor.temperature()  
+    dht_temp = dht_sensor.temperature()  
     dht_humidity = dht_sensor.humidity() 
 
     # Control relay and alert pattern based on gas sensor value
@@ -25,16 +44,24 @@ while True:
         play_gas_alert(cycles=2)
         stop_buzzer()
         RELAY.on()
+        gas_state = "!!GAS LEAK DETECTED!!"
 
     else:
         LED.off()
         stop_buzzer()
         RELAY.off()
+        gas_state = "Gas levels normal"
     
     # Print sensor values for debugging
     print("Gas Sensor Value:", gas_value)
     print("DHT Humidity Value:", dht_humidity)
-    print("DHT Temperature Value:", dht_value)
-    
+    print("DHT Temperature Value:", dht_temp)
+
+    # Send sensor values to Blynk
+    blynk.run()  # Process Blynk events
+    blynk.virtual_write(0, gas_state)  # Send gas sensor value to virtual pin V1
+    blynk.virtual_write(1, dht_humidity)  # Send humidity value to virtual pin V2
+    blynk.virtual_write(2, dht_temp)  # Send temperature value to virtual pin V3
+
     # Wait for a second before the next reading
     time.sleep(1)
